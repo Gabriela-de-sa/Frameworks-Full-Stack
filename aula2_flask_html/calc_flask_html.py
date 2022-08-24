33af9102237e4c33e6a306f61d3bc81093b4ea46
Gabriela-de-sa <g.208gabs@gmail.com>
2022-08-23 21:18:41 -0300

    aula2

--- aula2_flask_html/calc_flask_html.py
+++ aula2_flask_html/calc_flask_html.py
@@ -30,4 +30,5 @@ def calcula():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5005))
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='127.0.0.1', port=port)   
+    
